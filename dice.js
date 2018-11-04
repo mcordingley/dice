42b526afdcb62ecc8c149e385a1ff33ad9448ba3
Michael Cordingley <Michael.Cordingley@gmail.com>
2018-11-04 15:10:49 -0500

    Remove individual dice fields.

--- dice.js
+++ dice.js
@@ -1,43 +1,14 @@
 import Parser from './Parser.js';
 import Tokenizer from './Tokenizer.js';
 
-window.Tokenizer = Tokenizer;
-
-function readIntFromInput(element) {
-    const value = element.value ? parseInt(element.value, 10) : 0;
-
-    return isNaN(value) ? 0 : value;
-}
-
-function roll(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 const output = document.getElementById('output');
 
 document.getElementById('dice').addEventListener('submit', function (event) {
     event.preventDefault();
 
-    let total = 0;
-
-    ['d4', 'd6', 'd8', 'd10', 'd12', 'd20', 'd100'].forEach(function (id) {
-        const max = parseInt(id.substr(1), 10),
-            len = readIntFromInput(document.getElementById(id));
-
-        for (let i = 0; i < len; i++) {
-            total += roll(1, max);
-        }
-    });
-
-    total += readIntFromInput(document.getElementById('constant'));
-
-    if (document.getElementById('expression').value) {
-        total += (new Parser(new Tokenizer(document.getElementById('expression').value))).parse().evaluate();
-    }
-
     const node = document.createElement('p');
 
-    node.innerText = total;
+    node.innerText = (new Parser(new Tokenizer(document.getElementById('expression').value))).parse().evaluate();
 
     output.insertBefore(node, output.childNodes[0] || null);
 });
